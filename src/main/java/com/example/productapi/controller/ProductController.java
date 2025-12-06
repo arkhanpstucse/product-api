ca7307b26c66e26cc4e8bc6313325b9d9860bfd2
arkhanpstucse <abdur.rahman.khan.01502@gmail.com>
2025-12-06 20:31:29 +0600

    Resolved merge conflicts

--- src/main/java/com/example/productapi/controller/ProductController.java
+++ src/main/java/com/example/productapi/controller/ProductController.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/products")
 public class ProductController {
